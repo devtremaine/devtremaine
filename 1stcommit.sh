67076c8f7874988c508328c98711afe704de4834
devtremaine <124068625+devtremaine@users.noreply.github.com>
2023-02-21 16:00:47 -0600

    Add files via upload

---
+++ 1stcommit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hey whats you name?"
+read name
+echo "Is that So"
+read
+echo "Well Hello $name!!"
+read
+echo "So how is the weather today $name?"
+read
+echo "Well $name Prehaps you are right!"
